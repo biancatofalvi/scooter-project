be05418d1c515cb0a289886460f26a113439e6ff
Bianca Tofalvi <biancatofalvi@outlook.com>
2022-10-21 19:07:23 +0100

    created User class

--- src/User.js
+++ src/User.js
@@ -1,5 +1,27 @@
 class User {
   // User code here
+  #username
+  #password
+  #age
+
+  constructor(username, password, age) {
+    this.#username = username
+    this.#password = password
+    this.#age = age
+  }
+
+  getAge() {
+    return this.#age
+  }
+
+  getUsername() {
+    return this.#username
+  }
+
+  getPassword() {
+    return this.#password
+  }
+
 }
 
 module.exports = User
