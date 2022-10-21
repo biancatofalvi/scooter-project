99566f6ac0de8a64eafdfa34e67d7b6985b0d7e6
Bianca Tofalvi <biancatofalvi@outlook.com>
2022-10-21 17:33:59 +0100

    added User tests

--- tests/User.test.js
+++ tests/User.test.js
@@ -2,8 +2,26 @@ const User = require('../src/User')
 
 // User tests here
 
-// test username
 
-// test password
+describe('user properties', () => {
 
-// test age
+    const user1 = new User('mary', 'pass1', 19)
+
+    // test username
+    test('username is defined', () => {
+      // edit this to be a real test!
+      expect(user1.getUsername()).toBe('mary')
+    })
+
+    // test password
+    test('password is defined', () => {
+        // edit this to be a real test!
+        expect(user1.getPassword()).toBe('pass1')
+      })
+
+      // test age
+      test('age is defined', () => {
+        // edit this to be a real test!
+        expect(user1.getAge()).toBe(19)
+      })
+  })
